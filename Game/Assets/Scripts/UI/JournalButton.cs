1131b5671d3e775a40ec31100d73caee77bc8987
Kaltropis <Kaltropis@users.noreply.github.com>
2021-03-21 17:57:24 -0700

    Added Journal in MainSection and created testanim UI object to demonstrate Unity Animations

---
+++ Game/Assets/Scripts/UI/JournalButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalButton : MonoBehaviour
+{
+    Button journalButton;
+    [SerializeField] string journalBranch;
+    [SerializeField] int buttonPage;
+    [SerializeField] int targetPage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        journalButton = gameObject.GetComponent<Button>();
+        if(transform.parent.name == "Illnesses")
+        {
+            journalBranch = "Illness";
+        }
+        else
+        {
+            journalBranch = "Injury";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
