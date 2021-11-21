aa30c94eaa9724f8a1a113db7f46ae058f83619f
Kenny Averna <kaverna@student.ubc.ca>
2021-11-20 19:00:26 -0800

    Pause Menu (squashed)
    Deleted the quit and main menu buttons, having the resume one be the only one remaining, and then added the canvas to the main scene
    
    Pause Menu Script Update
    
    Added the serialized fields for the buttons and updated the implementation for the listeners.
    
    Update PauseMenu.cs
    
    Fixed the issue with having the pause menu not show up, it was in the Pause Menu animator settings, putting the pause menu update mode to unscaled time. The issue that remains is getting the button listeners to listen.
    
    Finished
    
    The finished paused menu has only the resume functionality.
    
    Actual Finished one
    
    added a }
    
    Pause Menu screen fader
    
    Cleaned up the hierarchy of the pauseMenu.cs and added a script fader, the two are completely implemented, just have to see how to organize them in the canvas.
    
    Updating Scenes
    
    Updating the scene elements to accommodate the scripts.
    
    Polished it up (FInished)
    
    added the fader animation for the pause menu and implemented the quit and load menu buttons. Also cleaned up the pause menu script by getting rid of the serialized button variables and attaching the buttons to the functions through the onClick option in each buttons inspector. Got rid of the fader script because of panel implementation.
    
    Co-Authored-By: Kenny Averna <68562756+kennyav@users.noreply.github.com>

--- Game/Assets/Scripts/UI/PauseMenu.cs
+++ Game/Assets/Scripts/UI/PauseMenu.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 /*
  * Handles the game pause menu
  * Author:  Kenny A @ 2021-11-06
  */
+
 public class PauseMenu : MonoBehaviour
 {
-   GameObject[] pauseObjects;
+   public GameObject pauseMenuUI;
+   public bool gameIsPaused = false;
+   // making a public gameIsPaused allows other scripts to run functions depending on 
+   // whether the game is pause.
 
    // Start is called before the first frame update
    void Start()
@@ -16,37 +22,47 @@ public class PauseMenu : MonoBehaviour
         // If the time scale is at 0, the game is paused
         // If the time scale is greater than 0, the game is un-paused
         Time.timeScale = 1; 
-        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
-        hidePauseMenu();
-        
+        resume();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Uses the p button to pause and unpause the game
-        if(Input.GetKeyDown(KeyCode.P)) {
+        // Uses the Escape button to pause and unpause the game
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Escape key was pressed");
             if(Time.timeScale == 1) {
-                Time.timeScale = 0;
-                loadPauseMenu();
+                pause();
             } else if (Time.timeScale == 0) {
-                Debug.Log("high");
-                Time.timeScale = 1;
-                hidePauseMenu();
+                resume();
             }
         }
         
     }
 
-    public void loadPauseMenu() {
-        foreach(GameObject i in pauseObjects) {
-            i.SetActive(true);
-        }
+// loadPauseMenu sets the pause menu UI panel to true, making it show in game, and sets the 
+// time scale to 0, which pauses the game. We then update the gameIsPaused variable to true.
+    public void pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
     }
 
-    public void hidePauseMenu() {
-        foreach(GameObject i in pauseObjects) {
-             i.SetActive(false);
-        }
+// hidePauseMenu sets the pause menu UI panel to false, hiding the panel from the player, resumes 
+// the game by setting the time scale to 1, and updates the gameIsPaused variable to false.
+    public void resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
     }
+
+    // public void loadMenu() {
+    //     Time.timeScale = 1f;
+    //     SceneManager.LoadScene("Menu");
+    // }
+
+    // public void quitGame() {
+    //     Debug.Log("quitting game...");
+    //     Application.Quit();
+    // }
 }
