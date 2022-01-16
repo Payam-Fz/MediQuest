852a268d9aa1a05d315b6a32a042e22209b5a392
Kenny Averna <kaverna@student.ubc.ca>
2022-01-15 17:30:46 -0800

    Settings Menus
    Created a test scene for the settings menu. Started on the layout with the three sliders and back button. Created a Settings script and began implementing it.

---
+++ Game/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void Back() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void MasterSlider(float value) {
+
+    }
+
+    public void MusicSlider(float value) {
+
+    }
+
+    public void SFXSlider(float value) {
+
+    }
+}
