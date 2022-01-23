9eae70501c084340a8316861cac8ffd4792f0317
YanNaingWin1 <ynwin1@student.ubc.ca>
2022-01-22 18:03:49 -0800

    [1] Clock In Clock Out Implementation
    - ClockMachineInteractManager -> functions implemented
    - created Clock Menu and Canvas

---
+++ Game/Assets/Scripts/Interactive Objects/ClockMachineInteractManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockMachineInteractManager : MonoBehaviour, IInteractable
+{
+    SpriteRenderer clockSprite;
+    [SerializeField] Sprite clockOn;
+    [SerializeField] Sprite clockOff;
+    bool playerPresent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clockSprite = GetComponent<SpriteRenderer>();
+        
+    }
+
+    public void Interact()
+    {
+        if (clockSprite.sprite == clockOff)
+        {
+            clockSprite.sprite = clockOn;
+            if (playerPresent)
+            {
+                //
+            }
+        }
+        else if (clockSprite.sprite == clockOn)
+        {
+            clockSprite.sprite = clockOff;
+            //
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player is at the clock machine");
+
+        if (collision.tag == "PlayerTag")
+        {
+            playerPresent = true;
+            if (clockSprite.sprite == clockOn)
+            {
+                //
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerTag")
+        {
+            playerPresent = false;
+            //
+        }
+    }
+
+    public void ManualHighlight()
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Clock Highlight is not implemented.");
+    }
+
+}
