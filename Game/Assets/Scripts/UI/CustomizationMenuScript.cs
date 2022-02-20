bab4b708c18a6d7edc48a7222afbd1c8000688dc
trevrasher <trevrasher12@gmail.com>
2022-02-19 16:40:38 -0800

    [2] Player Customization Menu
    Finished all scripts/sliders/buttons, need to make a proper UI with them after characterInfo is redone

--- Game/Assets/Scripts/UI/CustomizationMenuScript.cs
+++ Game/Assets/Scripts/UI/CustomizationMenuScript.cs
@@ -12,10 +12,12 @@ public class CustomizationMenuScript : MonoBehaviour
     public Slider eyeSlider;
     public TMP_InputField nameBox;
 
+    public CharacterInfo[] charInfo;
 
 
-    public float hairVal;   
-    public float skinVal;
+
+    public int hairVal;   
+    public int skinVal;
     public float eyeVal;
     public string name;
     // Start is called before the first frame update
@@ -25,14 +27,20 @@ public class CustomizationMenuScript : MonoBehaviour
         hairSlider = GameObject.Find("HairSlider").GetComponent<Slider>();
         eyeSlider = GameObject.Find("EyeSlider").GetComponent<Slider>();
         nameBox = GameObject.Find("NameBox").GetComponent<TMP_InputField>();
+
+        charInfo = Resources.LoadAll<CharacterInfo>("Data/Character/Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        skinVal = skinSlider.value;
-        hairVal = hairSlider.value;
+        skinVal = (int)skinSlider.value;
+        hairVal = (int)hairSlider.value;
         eyeVal = eyeSlider.value;
         name = nameBox.text;
+
+        charInfo[0].Name = name;
+        charInfo[0].hairColor = hairVal;
+        charInfo[0].skinColor = skinVal;
     }
 }
---
+++ Game/Assets/Scripts/UI/GenderButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+ 
+public class GenderButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+ 
+private bool buttonPressed;
+public Gender genderValue;
+private CharacterInfo[] charInfo;
+
+
+void Start() {
+    charInfo = Resources.LoadAll<CharacterInfo>("Data/Character/Player");
+}
+ 
+public void OnPointerDown(PointerEventData eventData){
+    buttonPressed = true;
+    charInfo[0].gender = genderValue;
+
+}
+ 
+public void OnPointerUp(PointerEventData eventData){
+    buttonPressed = false;
+}
+}
