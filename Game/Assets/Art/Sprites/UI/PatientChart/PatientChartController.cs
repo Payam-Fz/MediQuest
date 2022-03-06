0dde023a5f94c6ce3cbe7455f0173bcc6bca98ea
YanNaingWin1 <ynwin1@student.ubc.ca>
2022-03-05 16:13:45 -0800

    [4] Patient Chart
    - Implemented Activation and Deactivation

--- Game/Assets/Art/Sprites/UI/PatientChart/PatientChartController.cs
+++ Game/Assets/Art/Sprites/UI/PatientChart/PatientChartController.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatientChartScript : MonoBehaviour
+public class PatientChartController : MonoBehaviour
 {
+    Canvas patientChart;
+    [SerializeField] CharacterInfo patientBackground;
+    [SerializeField] DiagnosisProgress patientProgress;
+    [SerializeField] MedicalInfo patientMedicalInfo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        patientChart = GetComponent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FillPatientBackground()
+    {
+
+    }
+
+    void Activate()
+    {
+        Debug.Log("Patient Chart has been activated");
+        patientChart.gameObject.SetActive(true);
+    }
+
+    void Deactivate()
+    {
+        Debug.Log("Patient Chart has been deactivated");
+        patientChart.gameObject.SetActive(false);
     }
 }
