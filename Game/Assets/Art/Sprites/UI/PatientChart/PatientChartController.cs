5f10dc342ddd0fba36d38c8be563fa0514d4be0a
YanNaingWin1 <ynwin1@student.ubc.ca>
2022-03-05 18:24:08 -0800

    [5] Patient Char
    - added button on the chart
    - partially implemented filling information mechanism
    - updated activation and deactivation functions

--- Game/Assets/Art/Sprites/UI/PatientChart/PatientChartController.cs
+++ Game/Assets/Art/Sprites/UI/PatientChart/PatientChartController.cs
@@ -1,40 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PatientChartController : MonoBehaviour
 {
-    Canvas patientChart;
+    public Behaviour Chart_Canvas;
     [SerializeField] CharacterInfo patientBackground;
+    [SerializeField] CharacterInfo doctorBackground;
     [SerializeField] DiagnosisProgress patientProgress;
     [SerializeField] MedicalInfo patientMedicalInfo;
 
     // Start is called before the first frame update
     void Start()
     {
-        patientChart = GetComponent<Canvas>();
+        FillPatientBackground();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 
     void FillPatientBackground()
     {
+        //string dateTime
+        string physician = doctorBackground.Name;
+        string name = patientBackground.Name;
+        string age = patientBackground.age.ToString();
+        string sex = patientBackground.gender.ToString();
+        //string complaint;
+        //string illness;
+        //string history;
+        //string test;
+        //string dx;
+        //string plan;
 
+        Transform physician_name = transform.Find("Physician");
+        physician_name.GetComponent<Text>().text = physician;
+
+        Transform patient_name = transform.Find("Name");
+        patient_name.GetComponent<Text>().text = name;
+
+        Transform patient_age = transform.Find("Age");
+        patient_age.GetComponent<Text>().text = age;
+
+        Transform patient_sex = transform.Find("Sex");
+        patient_sex.GetComponent<Text>().text = sex;
     }
 
     void Activate()
     {
         Debug.Log("Patient Chart has been activated");
-        patientChart.gameObject.SetActive(true);
+        Chart_Canvas.GetComponent<Canvas>().gameObject.SetActive(true);
     }
 
     void Deactivate()
     {
         Debug.Log("Patient Chart has been deactivated");
-        patientChart.gameObject.SetActive(false);
+        Chart_Canvas.GetComponent<Canvas>().gameObject.SetActive(true);
     }
 }
