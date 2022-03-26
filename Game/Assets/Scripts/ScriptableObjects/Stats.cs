274a17a73b57f0df50e6e1251eb7c6e0c4aae7e1
payam <payam.4uzandeh@gmail.com>
2022-03-26 13:02:55 -0700

    [2] Score Counter
    Final

--- Game/Assets/Scripts/ScriptableObjects/Stats.cs
+++ Game/Assets/Scripts/ScriptableObjects/Stats.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Stats_player", menuName = "CodeBlue/Player stats")]
+//[CreateAssetMenu(fileName = "Stats_player", menuName = "CodeBlue/Player stats")]
 public class Stats : MonoBehaviour
 {
-    public int totalScore;
+    public int totalScore = 0;
 
     // Start is called before the first frame update
     void Start()
---
+++ Game/Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    //[SerializeField] TextMeshProUGUI totalScoreText;
+    //[SerializeField] public int totalScore;
+    [SerializeField] Stats playerStats;
+    TextMeshProUGUI totalScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalScoreText = gameObject.GetComponent<TextMeshProUGUI>();
+        totalScoreText.text = "Score: " + playerStats.totalScore;
+    }
+
+    // Update is called once per frame
+    void UpdateScoreText()
+    {
+        totalScoreText.text = "Score: " + playerStats.totalScore;
+    }
+
+
+}
