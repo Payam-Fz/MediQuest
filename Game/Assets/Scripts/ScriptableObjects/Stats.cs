55f34d572e4ccd1efa18b84b24f4c2e7cf277dce
LeArill <93508295+LeArill@users.noreply.github.com>
2022-03-25 23:07:30 -0700

    Score counter display
    score counter display

---
+++ Game/Assets/Scripts/ScriptableObjects/Stats.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stats_player", menuName = "CodeBlue/Player stats")]
+public class Stats : MonoBehaviour
+{
+    public int totalScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void increaseScore(int amount) 
+    {
+        this.totalScore += amount;
+    }
+
+    public void decreaseScore(int amount)
+    {
+        this.totalScore -= amount;
+    }
+}
